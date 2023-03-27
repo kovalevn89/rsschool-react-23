c423a25daee96238922311316a71f126c4d77ad6
Kovalev Nikolay <kovalev.nikolay.89@gmail.com>
2023-03-27 17:11:45 +0300

    feat: add form submit function

--- rsschool-react/src/pages/Inputpage.tsx
+++ rsschool-react/src/pages/Inputpage.tsx
@@ -5,8 +5,17 @@ import InputData from '../components/inputpage/InputData';
 import InputSex from '../components/inputpage/InputSex';
 import InputFile from '../components/inputpage/InputFile';
 import InputAccept from '../components/inputpage/InputAccept';
+import Card from '../components/card/Card';
 
-class Inputpage extends React.Component<{}, { text: string }> {
+interface ICard {
+  brand: string;
+  name: string;
+  date: string;
+  gender: string;
+  image: string;
+}
+
+class Inputpage extends React.Component<{}> {
   private inputBrand: React.RefObject<HTMLSelectElement>;
   private inputBrandError: React.RefObject<HTMLDivElement>;
   private inputName: React.RefObject<HTMLInputElement>;
@@ -20,6 +29,9 @@ class Inputpage extends React.Component<{}, { text: string }> {
   private inputFileError: React.RefObject<HTMLDivElement>;
   private inputAccept: React.RefObject<HTMLInputElement>;
   private inputAcceptError: React.RefObject<HTMLDivElement>;
+  private cardListRef: React.RefObject<HTMLDivElement>;
+
+  state: { cardList: ICard[] };
 
   constructor(props: Readonly<{}>) {
     super(props);
@@ -36,105 +48,130 @@ class Inputpage extends React.Component<{}, { text: string }> {
     this.inputFileError = React.createRef();
     this.inputAccept = React.createRef();
     this.inputAcceptError = React.createRef();
+    this.cardListRef = React.createRef();
+    this.state = { cardList: [] };
   }
 
   render() {
     return (
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
+      <div>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
 
-          if (this.inputBrand.current !== null) {
-            console.log('Choose a BRAND: ', this.inputBrand.current.value);
+            let isError = false;
 
-            if (this.inputBrandError.current !== null) {
-              if (this.inputBrand.current.value === 'choose') {
-                this.inputBrandError.current.innerHTML = 'ERROR! please choose brand!';
-              } else {
-                this.inputBrandError.current.innerHTML = '';
+            if (this.inputBrand.current !== null) {
+              if (this.inputBrandError.current !== null) {
+                if (this.inputBrand.current.value === 'choose') {
+                  this.inputBrandError.current.innerHTML = 'ERROR! please choose brand!';
+                  isError = true;
+                } else {
+                  this.inputBrandError.current.innerHTML = '';
+                }
               }
             }
-          }
 
-          if (this.inputName.current !== null) {
-            console.log('Product name: ', this.inputName.current.value);
-
-            if (this.inputNameError.current !== null) {
-              if (this.inputName.current.value === '') {
-                this.inputNameError.current.innerHTML = 'ERROR! please enter product name!';
-              } else {
-                this.inputNameError.current.innerHTML = '';
+            if (this.inputName.current !== null) {
+              if (this.inputNameError.current !== null) {
+                if (this.inputName.current.value === '') {
+                  this.inputNameError.current.innerHTML = 'ERROR! please enter product name!';
+                  isError = true;
+                } else {
+                  this.inputNameError.current.innerHTML = '';
+                }
               }
             }
-          }
 
-          if (this.inputDate.current !== null) {
-            console.log('Planned date of arrival at the warehouse: ', this.inputDate.current.value);
-
-            if (this.inputDateError.current !== null) {
-              if (this.inputDate.current.value === '') {
-                this.inputDateError.current.innerHTML = 'ERROR! please choose arrival date!';
-              } else {
-                this.inputDateError.current.innerHTML = '';
+            if (this.inputDate.current !== null) {
+              if (this.inputDateError.current !== null) {
+                if (this.inputDate.current.value === '') {
+                  this.inputDateError.current.innerHTML = 'ERROR! please choose arrival date!';
+                  isError = true;
+                } else {
+                  this.inputDateError.current.innerHTML = '';
+                }
               }
             }
-          }
 
-          if (this.inputSexMale.current !== null && this.inputSexFemale.current !== null) {
-            console.log('Gender 1: ', this.inputSexMale.current.checked);
-            console.log('Gender 2: ', this.inputSexFemale.current.checked);
-
-            if (this.inputSexError.current !== null) {
-              if (
-                this.inputSexMale.current.checked === false &&
-                this.inputSexFemale.current.checked === false
-              ) {
-                this.inputSexError.current.innerHTML = 'ERROR! please choose gender!';
-              } else {
-                this.inputSexError.current.innerHTML = '';
+            if (this.inputSexMale.current !== null && this.inputSexFemale.current !== null) {
+              if (this.inputSexError.current !== null) {
+                if (
+                  this.inputSexMale.current.checked === false &&
+                  this.inputSexFemale.current.checked === false
+                ) {
+                  this.inputSexError.current.innerHTML = 'ERROR! please choose gender!';
+                  isError = true;
+                } else {
+                  this.inputSexError.current.innerHTML = '';
+                }
               }
             }
-          }
 
-          if (this.inputFile.current !== null) {
-            console.log('Image file: ', this.inputFile.current.value);
-
-            if (this.inputFileError.current !== null) {
-              if (this.inputFile.current.value === '') {
-                this.inputFileError.current.innerHTML = 'ERROR! please choose file!';
-              } else {
-                this.inputFileError.current.innerHTML = '';
+            if (this.inputFile.current !== null) {
+              if (this.inputFileError.current !== null) {
+                if (this.inputFile.current.value === '') {
+                  this.inputFileError.current.innerHTML = 'ERROR! please choose file!';
+                  isError = true;
+                } else {
+                  this.inputFileError.current.innerHTML = '';
+                }
               }
             }
-          }
 
-          if (this.inputAccept.current !== null) {
-            console.log('Accept upload: ', this.inputAccept.current.checked);
-
-            if (this.inputAcceptError.current !== null) {
-              if (this.inputAccept.current.checked === false) {
-                this.inputAcceptError.current.innerHTML = 'ERROR! please accept info!';
-              } else {
-                this.inputAcceptError.current.innerHTML = '';
+            if (this.inputAccept.current !== null) {
+              if (this.inputAcceptError.current !== null) {
+                if (this.inputAccept.current.checked === false) {
+                  this.inputAcceptError.current.innerHTML = 'ERROR! please accept info!';
+                  isError = true;
+                } else {
+                  this.inputAcceptError.current.innerHTML = '';
+                }
               }
             }
-          }
-        }}
-      >
-        <h2>Input product card info:</h2>
-        <BrandSelect brandref={this.inputBrand} brandErrorRef={this.inputBrandError} />
-        <InputProduct anyref={this.inputName} nameErrorRef={this.inputNameError} />
-        <InputData anyref={this.inputDate} dataErrorRef={this.inputDateError} />
-        <InputSex
-          maleRef={this.inputSexMale}
-          femaleRef={this.inputSexFemale}
-          sexErrorRef={this.inputSexError}
-        />
-        <InputFile fileRef={this.inputFile} fileErrorRef={this.inputFileError} />
-        <InputAccept acceptRef={this.inputAccept} acceptErrorRef={this.inputAcceptError} />
 
-        <button type="submit">Submit</button>
-      </form>
+            if (isError === false) {
+              const card: ICard = {
+                brand: this.inputBrand.current!.value,
+                name: this.inputName.current!.value,
+                date: this.inputDate.current!.value,
+                gender: this.inputSexMale.current!.checked ? 'male' : 'female',
+                image: this.inputFile.current!.value,
+              };
+
+              const temp = this.state.cardList;
+              temp.push(card);
+              this.setState({ cardList: temp });
+            }
+          }}
+        >
+          <h2>Input product card info:</h2>
+          <BrandSelect brandref={this.inputBrand} brandErrorRef={this.inputBrandError} />
+          <InputProduct anyref={this.inputName} nameErrorRef={this.inputNameError} />
+          <InputData anyref={this.inputDate} dataErrorRef={this.inputDateError} />
+          <InputSex
+            maleRef={this.inputSexMale}
+            femaleRef={this.inputSexFemale}
+            sexErrorRef={this.inputSexError}
+          />
+          <InputFile fileRef={this.inputFile} fileErrorRef={this.inputFileError} />
+          <InputAccept acceptRef={this.inputAccept} acceptErrorRef={this.inputAcceptError} />
+
+          <button type="submit">Submit</button>
+        </form>
+        <div ref={this.cardListRef}>
+          {this.state.cardList.map((item, index) => (
+            <Card
+              key={String(index + 1)}
+              mkey={String(index + 1)}
+              thumbnail={item.image}
+              manufacturer={item.brand}
+              title={item.name}
+              price={String(0)}
+            />
+          ))}
+        </div>
+      </div>
     );
   }
 }
