86f167410e43e3df25cc05b9d3cc0156cf3c0b53
Niklay Kovalev <kovalev.nikolay.89@gmail.com>
2023-03-31 01:00:08 +0300

    feat: add sass and more style

--- rsschool-react/src/App.tsx
+++ rsschool-react/src/App.tsx
@@ -1,5 +1,5 @@
-import './App.css';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import './App.scss';
+import { Routes, Route, NavLink, useLocation } from 'react-router-dom';
 
 import Mainpage from './pages/Mainpage';
 import Aboutpage from './pages/Aboutpage.jsx';
@@ -7,33 +7,42 @@ import Errorpage from './pages/Errorpage.jsx';
 import Inputpage from './pages/Inputpage.jsx';
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <header>
-        <NavLink
-          to="/"
-          className={({ isActive, isPending }) =>
-            isPending ? 'pending' : isActive ? 'active' : ''
-          }
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/input"
-          className={({ isActive, isPending }) =>
-            isPending ? 'pending' : isActive ? 'active' : ''
-          }
-        >
-          Input Form
-        </NavLink>
-        <NavLink
-          to="/about"
-          className={({ isActive, isPending }) =>
-            isPending ? 'pending' : isActive ? 'active' : ''
-          }
-        >
-          About
-        </NavLink>
+        <div className="logo__box">
+          <p>MovieBox</p>
+        </div>
+        <div className="current__page">
+          <p>{location.pathname}</p>
+        </div>
+        <div className="nav__menu">
+          <NavLink
+            to="/"
+            className={({ isActive, isPending }) =>
+              isPending ? 'pending' : isActive ? 'active' : ''
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/input"
+            className={({ isActive, isPending }) =>
+              isPending ? 'pending' : isActive ? 'active' : ''
+            }
+          >
+            Form
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive, isPending }) =>
+              isPending ? 'pending' : isActive ? 'active' : ''
+            }
+          >
+            About
+          </NavLink>
+        </div>
       </header>
       <main>
         <Routes>
